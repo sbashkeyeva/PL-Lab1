d34b18f840f67a14dccec78fa4f42e2d659b9145
sbashkeyeva <sbashkeyeva@gmail.com>
2016-09-24 09:06:18 +0600

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string firstname;
+    string lastname;
+	cout<<"What's your firstname?"<<endl;
+	cin>>firstname;
+	cout<<"What's your lastname?"<<endl;
+	cin>>lastname;
+	cout<<"Hello! "<<firstname<<" "<<lastname;
+	return 0;
+}
---
+++ b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+	int a,b,S,D,P;
+	cin>>a>>b;
+	S=a+b;
+	cout<<S<<endl;
+	cin>>a>>b;
+    D=a-b;
+    cout<<D<<endl;
+    cin>>a>>b;
+    P=a*b;
+    cout<<P<<endl;
+
+
+
+return 0;
+}
